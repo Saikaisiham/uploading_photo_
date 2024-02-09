ec26b1d52b9a058856c443548d62524ed857a235
Saikaisiham <blackbutlersiham2001@gmail.com>
2024-02-09 17:54:57 +0100

    commit

--- main/settings.py
+++ main/settings.py
@@ -80,10 +80,15 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'uploading_image',
+        'USER': 'uploading_project_admin',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
+
 
 
 # Password validation
